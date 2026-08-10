3e0f3d2ae627792c734d538e4c38e9a88f02e9fe
ehsanpaknahad <packnahad@gmail.com>
2026-08-10 11:01:46 +0330

    stick the toolbat and avoid scrolling

--- src/components/GeometryTable.tsx
+++ src/components/GeometryTable.tsx
@@ -21,7 +21,7 @@ function GeometryTable({ coordinates, onVertexClick }) {
   };
 
   return (
-    <div>
+    <div className="geometry-table-container">
       <div className="geometry-table-toolbar">
         {!isEditMode ? (
           <calcite-button
@@ -53,37 +53,38 @@ function GeometryTable({ coordinates, onVertexClick }) {
           </>
         )}
       </div>
-
-      <calcite-table
-        bordered
-        current-page="0"
-        interaction-mode="interactive"
-        layout="auto"
-        numbered
-        page-size="0"
-        scale="s"
-        striped
-        selection-mode={isEditMode ? "single" : "none"}
-      >
-        <calcite-table-row slot="table-header">
-          <calcite-table-header heading="Easting"></calcite-table-header>
-          <calcite-table-header heading="Northing"></calcite-table-header>
-          <calcite-table-header heading="Elevation"></calcite-table-header>
-        </calcite-table-row>
-
-        {coordinates.map((coord, index) => (
-          <calcite-table-row
-            key={index}
-            onClick={() => isEditMode && onVertexClick(coord, index)}
-          >
-            <calcite-table-cell>{coord[0].toFixed(3)}</calcite-table-cell>
-            <calcite-table-cell>{coord[1].toFixed(3)}</calcite-table-cell>
-            <calcite-table-cell>
-              {coord[2]?.toFixed(3) ?? "0.000"}
-            </calcite-table-cell>
+      <div className="geometry-table-scroll">
+        <calcite-table
+          bordered
+          current-page="0"
+          interaction-mode="interactive"
+          layout="auto"
+          numbered
+          page-size="0"
+          scale="s"
+          striped
+          selection-mode={isEditMode ? "single" : "none"}
+        >
+          <calcite-table-row slot="table-header">
+            <calcite-table-header heading="Easting"></calcite-table-header>
+            <calcite-table-header heading="Northing"></calcite-table-header>
+            <calcite-table-header heading="Elevation"></calcite-table-header>
           </calcite-table-row>
-        ))}
-      </calcite-table>
+
+          {coordinates.map((coord, index) => (
+            <calcite-table-row
+              key={index}
+              onClick={() => isEditMode && onVertexClick(coord, index)}
+            >
+              <calcite-table-cell>{coord[0].toFixed(3)}</calcite-table-cell>
+              <calcite-table-cell>{coord[1].toFixed(3)}</calcite-table-cell>
+              <calcite-table-cell>
+                {coord[2]?.toFixed(3) ?? "0.000"}
+              </calcite-table-cell>
+            </calcite-table-row>
+          ))}
+        </calcite-table>
+      </div>
     </div>
   );
 }
